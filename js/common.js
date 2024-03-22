7dc2588975eab0dcc5c136afe847d2c5c5ad8d44
GGU940 <nayung30@naver.com>
2024-03-22 15:50:57 +0900

    메인비주얼 반응형 미완

---
+++ js/common.js
@@ -0,0 +1,10 @@
+
+let mainSwiper = new Swiper(".mainSwiper", {
+    effect: "cards",
+    // grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
