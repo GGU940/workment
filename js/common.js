8c01a477bd5008cf9f927d428e2f63b2f6b21cab
GGU940 <nayung30@naver.com>
2024-03-26 17:43:30 +0900

    메인비주얼 반응형 완. 스와이퍼 보더 이상함

--- js/common.js
+++ js/common.js
@@ -8,3 +8,42 @@ let mainSwiper = new Swiper(".mainSwiper", {
         disableOnInteraction: false,
     },
 });
+
+// const view = document.querySelector
+
+
+// let mainSwiper;
+
+// const initSwiper = () => {
+//     //화면 크기 800 이하
+//     if (window.innerWidth <= 880) {
+//         mainSwiper = new Swiper(".mainSwiper"), {
+//             effect: "fade", // 다른 이펙트를 적용
+//             loop: true,
+//             autoplay: {
+//                 delay: 3000,
+//                 disableOnInteraction: false,
+//             },
+//         };
+//     } else {
+//         mainSwiper = new Swiper(".mainSwiper", {
+//             effect: "cards",
+//             loop: true,
+//             autoplay: {
+//                 delay: 3000,
+//                 disableOnInteraction: false,
+//             },
+//         });
+//     }
+// };
+
+// // 초기화 함수 호출
+// initSwiper();
+
+// // 화면 크기 변경 시 Swiper 객체 재설정
+// window.addEventListener('resize', () => {
+//     if (mainSwiper) {
+//         mainSwiper.destroy(); // 기존 Swiper 객체 파괴
+//     }
+//     initSwiper(); // Swiper 객체 다시 초기화
+// });
