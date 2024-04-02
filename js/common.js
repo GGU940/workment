ac8f7e93b51c3153bf74b77a2b11feaff21b7673
GGU940 <nayung30@naver.com>
2024-04-02 15:22:27 +0900

    ~ counter

--- js/common.js
+++ js/common.js
@@ -12,18 +12,10 @@ let mainSwiper = new Swiper(".mainSwiper", {
 
 
 
-
-// //스크롤 될 텍스트바 하나 더 추가
-// const copyText = $wideScrollText.cloneNode(true);//요소 복사
-// $scrollText.appendChild(copyText);
-// // $scrollText.appendChild(copyText);
-
 const $scrollText = document.querySelector('.scrollText')
 const $wideScrollText = document.querySelector('.wideScrollText');
 
-
-let Scrollwidth = $wideScrollText.clientWidth;
-console.log(Scrollwidth);
+let Scrollwidth = $wideScrollText.clientWidth;//전체 길이
 
 let count = 0;
 function marqueeText(count, element, direction) {
@@ -31,32 +23,30 @@ function marqueeText(count, element, direction) {
         element.style.transform = 'translateX(0)';
         count = 0;
     }
+
     element.style.transform = `translateX( ${count * direction}px )`
     return count;
+
 }
 
 function animate() {
     count++;
     count = marqueeText(count, $wideScrollText, -1);
 
-    window.requestAnimationFrame(animate)//에니메이션을 지속적으로 업데이트. 재페인팅 되면서 계속 위치가 바뀌는듯
+    window.requestAnimationFrame(animate)//에니메이션을 지속적으로 업데이트. 
+    //재페인팅 되면서 계속 위치가 바뀌는듯
+
+
 }
 
 //스크롤하면 더 빨리 넘어감
 window.addEventListener('scroll', () => {
-    count += 10;
+    count += 5;
 })
 
 animate();
 
 //hover중일때 멈추게 하고 시픔..
-
-function isMouseover(element) {
-    element.addEventListener('mouseover', () => {
-        return true;
-    });
-    return false;
-};
 
 // do { animate(); }
 // while (isMouseover($wideScrollText) == false)
