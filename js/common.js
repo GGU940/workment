9c58e08f3953ea3ff7324c82a145ce8921560ef7
GGU940 <nayung30@naver.com>
2024-04-03 15:37:20 +0900

    ~together?

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
 
+
+
 let mainSwiper = new Swiper(".mainSwiper", {
     effect: "cards",
     // grabCursor: true,
@@ -10,7 +12,7 @@ let mainSwiper = new Swiper(".mainSwiper", {
 });
 
 
-
+//-----------------------scrollText  시작-----------------------
 
 const $scrollText = document.querySelector('.scrollText')
 const $wideScrollText = document.querySelector('.wideScrollText');
@@ -46,7 +48,60 @@ window.addEventListener('scroll', () => {
 
 animate();
 
-//hover중일때 멈추게 하고 시픔..
+//-----------------------scrollText  끝-----------------------
 
-// do { animate(); }
-// while (isMouseover($wideScrollText) == false)
+
+//-----------------------testimonial  시작-----------------------
+// .page01 누르면 slideCard1:first 2:middle 3:last
+// .page02 누르면 slideCard2:first 3:middle 1:last
+// .page03 누르면 slideCard3:first 1:middle 2:last
+
+// 눌린 page00: first 추가 
+// 눌린 것의 previousElementSibling : last 추가
+// 눌린 것의 nextElementSibling : middle 추가
+const slideCards = document.querySelectorAll('.slideCard');
+const slideCard01 = document.querySelector('.slideCard:nth-of-type(1)');
+const slideCard02 = document.querySelector('.slideCard:nth-of-type(2)');
+const slideCard03 = document.querySelector('.slideCard:nth-of-type(3)');
+// console.log(slideCards[0]);
+// slideCards.forEach((element) => {
+//     console.log(element.nextElementSibling.nextElementSibling);
+// });
+const pageBtn = document.querySelectorAll('.page>span')
+const pageBtn01 = document.querySelector('.page01');
+const pageBtn02 = document.querySelector('.page02');
+const pageBtn03 = document.querySelector('.page03');
+
+let willFirst;
+
+pageBtn.forEach((ele) => {
+    ele.addEventListener('click', (e) => {
+        slideCards.forEach(card => {
+            card.classList.remove('first', 'middle', 'last');
+        });
+        // console.log(e.target);
+        if (e.target === pageBtn01) { willFirst = slideCard01 }
+        else if (e.target === pageBtn02) { willFirst = slideCard02 }
+        else if (e.target === pageBtn03) { willFirst = slideCard03 }
+        // console.log(willFirst);
+        willFirst.classList.add('first');
+
+        if (willFirst.nextElementSibling) {
+            willFirst.nextElementSibling.classList.add('middle');
+        }
+        else {
+            willFirst.previousElementSibling.previousElementSibling.classList.add('middle');
+        }
+
+        if (willFirst.previousElementSibling) {
+            willFirst.previousElementSibling.classList.add('last');
+        }
+        else {
+            willFirst.nextElementSibling.nextElementSibling.classList.add('last');
+        }
+    });
+});
+
+
+
+//-----------------------testimonial  끝-----------------------
